41184e7629a4a3e4100ed0b8a3fa631171761bbd
Rubén Carretero <ruben.carretero@wolterskluwer.com>
2021-06-27 00:09:47 +0200

    Added console app

---
+++ Dados.Zombie.Console.App/Program.cs
@@ -0,0 +1,11 @@
+﻿namespace Dados.Zombie.Console.App
+{
+    using System;
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
